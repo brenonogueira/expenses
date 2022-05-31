a78907eec6dfe04fe3efe06add348d44248f1b79
breno nogueira <breno.nog.araujo@hotmail.com>
2022-05-31 18:02:38 -0400

    passando informações de componentes pai/filho filho/pai direta e indiretamente

---
+++ lib/components/transaction_form.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(Key? key, this.onSubmit) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(labelText: 'Título'),
+            ),
+            TextField(
+              controller: valueController,
+              decoration: InputDecoration(labelText: 'Valor R\$'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                    onPressed: () {
+                      final title = titleController.text;
+                      final value = double.tryParse(valueController.text) ?? 0.0;
+                      onSubmit(title, value);
+                    },
+                    child: Text(
+                      'Nova Transação',
+                      style: TextStyle(color: Colors.purple),
+                    )),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
